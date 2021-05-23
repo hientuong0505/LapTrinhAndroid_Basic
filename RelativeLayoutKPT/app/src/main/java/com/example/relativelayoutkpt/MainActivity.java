dc8f4df97f11c18c44d437052664969eb60477a7
hientuong0505 <hientuong4321@gmail.com>
2021-05-23 18:53:07 +0700

    Updated

--- RelativeLayoutKPT/app/src/main/java/com/example/relativelayoutkpt/MainActivity.java
+++ RelativeLayoutKPT/app/src/main/java/com/example/relativelayoutkpt/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.relativelayoutkpt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView txtNoiDung;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtNoiDung = (TextView) findViewById(R.id.txt1);
+        btn = (Button) findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtNoiDung.setText("Hellooooo!!!");
+            }
+        });
+
     }
 }
